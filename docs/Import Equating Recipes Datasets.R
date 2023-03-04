208b9ec12573a2ca8d5a0f0bfe3a956755dcc5e7
MichaelFinger <40723738+MichaelFinger@users.noreply.github.com>
2023-03-03 20:17:51 -0500

    meh

--- docs/Import Equating Recipes Datasets.R
+++ docs/Import Equating Recipes Datasets.R
@@ -1,28 +1,93 @@
-equatingRecipesExamplesFoldername = "~/Developer/EquatingRecipes/ER for distribution 9-10-09/Examples/"
+library(jsonlite)
+library(stringr)
 
-datasetFilenames = c("ConvRawSS2.in",
+equatingRecipesExamplesFoldername = "~/Developer/EquatingRecipes/ER for distribution 9-10-09/Examples/"
+equatingRecipesExamplesJsonFoldername = "~/Developer/EquatingRecipes/ER for distribution 9-10-09/Examples/JSON/"
+
+importFixedColumnsTextFile = function(filename) {
+  linesRead = trimws(readLines(filename))
+  
+  values = str_split(linesRead[1], pattern = " +|\t", simplify = TRUE)
+  
+  if (ncol(values) == 1) {
+    numberOfRows = as.numeric(linesRead[1])
+    
+    linesRead = linesRead[2:(numberOfRows + 1)] 
+  }
+  
+  values = str_split(linesRead, pattern = " +|\t", simplify = TRUE)
+  
+  columnsToKeep = colSums(values == "") < nrow(values)
+  rowsToKeep = rowSums(values == "") < ncol(values)
+  
+  values = values[rowsToKeep, columnsToKeep]
+  
+  dataset = NULL
+  for (columnIndex in 1:ncol(values)) {
+    if (sum(is.na(as.numeric(values[, columnIndex]))) == 0) {
+      if (columnIndex == 1) {
+        dataset = data.frame(V1 = as.numeric(values[, columnIndex]))
+      } else {
+        dataset = cbind(dataset,
+                        data.frame(V1 = as.numeric(values[, columnIndex])))  
+      }
+      
+    } else {
+      if (columnIndex == 1) {
+        dataset = data.frame(V1 = values[, columnIndex])
+      } else {
+        dataset = cbind(dataset,
+                   data.frame(V1 = values[, columnIndex]))
+      }
+    }
+    
+    colnames(dataset)[columnIndex] = paste0("V", columnIndex)
+  }
+  
+  return(dataset)
+}
+
+datasetFilenames = c("actmathfreq.dat",
                      "Conversion-reordered.DAT",
-                     "KB6Vitems",
-                     "KB6Xdist",
-                     "KB6Xdist.ST",
-                     "KB6Xitems",
-                     "KB6Xitems.ST",
-                     "KB6Ydist",
-                     "KB6Yitems",
-                     "actmathfreq.dat",
+                     "ConvRawSS2.in",
                      "dummyV1items",
+                     "dummyv2items",
                      "dummyV3items",
-                     "dummyXdist",
                      "dummyXdist.ST",
-                     "dummyXitems",
+                     "dummyXdist",
                      "dummyXitems.ST",
+                     "dummyXitems",
                      "dummyYdist",
                      "dummyYitems",
-                     "dummyv2items",
                      "fdexample.dat",
-                     "mondatx.dat",
-                     "mondaty.dat",
+                     "KB6Vitems",
+                     "KB6Xdist.ST",
+                     "KB6Xdist",
+                     "KB6Xitems.ST",
+                     "KB6Xitems",
+                     "KB6Ydist",
+                     "KB6Yitems",
                      "yctmath.TXT")
 
-index = 1
-readLines(paste0(equatingRecipesExamplesFoldername, datasetFilenames[index]))
+for (index in 1:length(datasetFilenames)) {
+  dataset = importFixedColumnsTextFile(paste0(equatingRecipesExamplesFoldername, datasetFilenames[index]))
+ 
+  json = toJSON(as.data.frame(dataset), dataframe = "values",matrix = "rowmajor", pretty = TRUE) 
+  
+  jsonFilename = paste0(equatingRecipesExamplesJsonFoldername, datasetFilenames[index], ".json")
+  
+  write(json, jsonFilename, append = FALSE)
+}
+
+dataset = read.fortran(paste0(equatingRecipesExamplesFoldername, "mondatx.dat"), 
+                       format = c("36I1", "2I2"))
+json = toJSON(as.data.frame(dataset), dataframe = "values",matrix = "rowmajor", pretty = TRUE) 
+jsonFilename = paste0(equatingRecipesExamplesJsonFoldername, "mondatx.dat", ".json")
+write(json, jsonFilename, append = FALSE)
+
+dataset = read.fortran(paste0(equatingRecipesExamplesFoldername, "mondaty.dat"), 
+                       format = c("36I1", "2I2"))
+json = toJSON(as.data.frame(dataset), dataframe = "values",matrix = "rowmajor", pretty = TRUE) 
+jsonFilename = paste0(equatingRecipesExamplesJsonFoldername, "mondaty.dat", ".json")
+write(json, jsonFilename, append = FALSE)
+
