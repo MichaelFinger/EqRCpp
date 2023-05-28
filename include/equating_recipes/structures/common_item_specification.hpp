adb8595cfd1dd82d19eb53c8d9168f9fb6a36dce
Michael Finger <40723738+MichaelFinger@users.noreply.github.com>
2023-05-28 13:13:37 -0400

    meh

--- include/equating_recipes/structures/common_item_specification.hpp
+++ include/equating_recipes/structures/common_item_specification.hpp
@@ -10,10 +10,12 @@
 #include <Eigen/Core>
 
 #include <equating_recipes/structures/irt_model.hpp>
+#include <equating_recipes/structures/item_specification.hpp>
 
 namespace EquatingRecipes {
   namespace Structures {
-    struct CommonItemSpecification {
+    class CommonItemSpecification {
+    public:
       int newID;
       int oldID;
       size_t numberOfCategories;
@@ -28,6 +30,28 @@ namespace EquatingRecipes {
       Eigen::VectorXd oldC;
       Eigen::VectorXd oldD;
       EquatingRecipes::Structures::IRTModel irtModel;
+
+      static CommonItemSpecification build(const EquatingRecipes::Structures::ItemSpecification& oldItemSpec,
+                                           const EquatingRecipes::Structures::ItemSpecification& newItemSpec) {
+        CommonItemSpecification commonItemSpec;
+
+        commonItemSpec.oldID = oldItemSpec.itemID;
+        commonItemSpec.newID = newItemSpec.itemID;
+        commonItemSpec.numberOfCategories = newItemSpec.numberOfCategories;
+        commonItemSpec.scalingConstant = newItemSpec.scalingConstant;
+        commonItemSpec.scoringFunctionValues = newItemSpec.scoringFunctionValues;
+        commonItemSpec.irtModel = newItemSpec.irtModel;
+        commonItemSpec.newA = newItemSpec.a;
+        commonItemSpec.newB = newItemSpec.b;
+        commonItemSpec.newC = newItemSpec.c;
+        commonItemSpec.newD = newItemSpec.d;
+        commonItemSpec.oldA = oldItemSpec.a;
+        commonItemSpec.oldB = oldItemSpec.b;
+        commonItemSpec.oldC = oldItemSpec.c;
+        commonItemSpec.oldD = oldItemSpec.d;
+
+        return commonItemSpec;
+      }
     };
   } // namespace Structures
 } // namespace EquatingRecipes
