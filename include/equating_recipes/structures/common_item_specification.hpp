043b19b64b8f95dd71f1821697e99d0457117259
MichaelFinger <40723738+MichaelFinger@users.noreply.github.com>
2023-06-04 19:31:10 -0400

    meh

--- include/equating_recipes/structures/common_item_specification.hpp
+++ include/equating_recipes/structures/common_item_specification.hpp
@@ -41,14 +41,63 @@ namespace EquatingRecipes {
         commonItemSpec.scalingConstant = newItemSpec.scalingConstant;
         commonItemSpec.scoringFunctionValues = newItemSpec.scoringFunctionValues;
         commonItemSpec.irtModel = newItemSpec.irtModel;
-        commonItemSpec.newA = newItemSpec.a;
-        commonItemSpec.newB = newItemSpec.b;
-        commonItemSpec.newC = newItemSpec.c;
-        commonItemSpec.newD = newItemSpec.d;
-        commonItemSpec.oldA = oldItemSpec.a;
-        commonItemSpec.oldB = oldItemSpec.b;
-        commonItemSpec.oldC = oldItemSpec.c;
-        commonItemSpec.oldD = oldItemSpec.d;
+
+        commonItemSpec.newA.setZero(commonItemSpec.numberOfCategories + 1);
+        commonItemSpec.newB.setZero(commonItemSpec.numberOfCategories + 1);
+        commonItemSpec.newC.setZero(commonItemSpec.numberOfCategories + 1);
+        commonItemSpec.newD.setZero(commonItemSpec.numberOfCategories + 1);
+
+        commonItemSpec.oldA.setZero(commonItemSpec.numberOfCategories + 1);
+        commonItemSpec.oldB.setZero(commonItemSpec.numberOfCategories + 1);
+        commonItemSpec.oldC.setZero(commonItemSpec.numberOfCategories + 1);
+        commonItemSpec.oldD.setZero(commonItemSpec.numberOfCategories + 1);
+        
+        switch (commonItemSpec.irtModel) {
+          case EquatingRecipes::Structures::IRTModel::THREE_PARAMETER_LOGISTIC:
+            commonItemSpec.newA(2) = newItemSpec.a(2);
+            commonItemSpec.newB(2) = newItemSpec.b(2);
+            commonItemSpec.newC(2) = newItemSpec.c(2);
+            commonItemSpec.oldA(2) = oldItemSpec.a(2);
+            commonItemSpec.oldB(2) = oldItemSpec.b(2);
+            commonItemSpec.oldC(2) = oldItemSpec.c(2);
+
+            break;
+          case EquatingRecipes::Structures::IRTModel::GRADED_RESPONSE:
+            commonItemSpec.newA(2) = newItemSpec.a(2);
+            commonItemSpec.oldA(2) = oldItemSpec.b(2);
+
+            for (size_t k = 2; k <= commonItemSpec.numberOfCategories; k++) {
+              commonItemSpec.newB(k) = newItemSpec.b(k);
+              commonItemSpec.oldB(k) = oldItemSpec.b(k);
+            }
+
+            break;
+          case EquatingRecipes::Structures::IRTModel::PARTIAL_CREDIT:
+            commonItemSpec.newA(2) = newItemSpec.a(2);
+            commonItemSpec.newB(0) = newItemSpec.b(0);
+            commonItemSpec.oldA(2) = oldItemSpec.a(2);
+            commonItemSpec.oldB(0) = oldItemSpec.b(0);
+
+            for (size_t k = 1; k <= commonItemSpec.numberOfCategories; k++) {
+              commonItemSpec.newB(k) = newItemSpec.b(k);
+              commonItemSpec.newD(k) = newItemSpec.d(k);
+              commonItemSpec.oldB(k) = oldItemSpec.b(k);
+              commonItemSpec.oldD(k) = oldItemSpec.d(k);
+            }
+            
+            break;
+          case EquatingRecipes::Structures::IRTModel::NOMINAL_RESPONSE:
+            for(size_t k = 1; k <= commonItemSpec.numberOfCategories; k++) {
+              commonItemSpec.newA(k) = newItemSpec.a(k);
+              commonItemSpec.newC(k) = newItemSpec.c(k);
+              commonItemSpec.oldA(k) = oldItemSpec.a(k);
+              commonItemSpec.oldC(k) = oldItemSpec.c(k);
+            }
+            
+            break;
+          default:
+            break;
+        }
 
         return commonItemSpec;
       }
